0fc7c9ea6f41ab62f19b330272d359895f909408
Ammar_Fahmy <40536975+AmmarFahmy@users.noreply.github.com>
2020-08-05 01:33:50 +0530

    MVC Applied

--- src/java/driverassapp/controller/RegisterServlet.java
+++ src/java/driverassapp/controller/RegisterServlet.java
@@ -3,9 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package driverassapp.model;
+package driverassapp.controller;
 
 import driverassapp.controller.ConnectionPro;
+import driverassapp.model.User;
+import driverassapp.model.UserDatabase;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
