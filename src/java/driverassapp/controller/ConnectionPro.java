5bcf9ef25cc618d90cbd45a0cf6ad7cbcbc06d31
Ammar_Fahmy <40536975+AmmarFahmy@users.noreply.github.com>
2020-08-05 01:14:42 +0530

    DB Connection provided as MVC

---
+++ src/java/driverassapp/controller/ConnectionPro.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driverassapp.controller;
+
+import java.sql.*;
+
+/**
+ *
+ * @author ammar
+ */
+public class ConnectionPro {
+    
+    private static Connection con;
+    
+    public static Connection getConnection(){
+        
+        try{
+            //Class.forName("com.mysql.jdbc.Driver");
+            
+            //Class.forName("org.apache.derby.jdbc.ClientDriver");
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            con=DriverManager.getConnection("jdbc:derby://localhost:1527/driverapp","ammar","ammar");
+            
+        }
+        catch(ClassNotFoundException | SQLException e){
+        }
+        return con;
+    }
+    
+}
