98ec369e35e30d53cc7e6a706f075a5bd8befd72
Ammar_Fahmy <40536975+AmmarFahmy@users.noreply.github.com>
2020-08-07 21:31:57 +0530

    User and Admin Dashboard added

--- src/java/driverassapp/model/UserDatabase.java
+++ src/java/driverassapp/model/UserDatabase.java
@@ -29,6 +29,7 @@ public class UserDatabase {
         try{
            //Insert register data to database
            String query = "insert into register(fullname,email,address,dob,register,password) values(?,?,?,?,?,?)";
+           String query1 = "insert into users(email,password) values(?,?)";
            
            PreparedStatement pt = this.con.prepareStatement(query);
            pt.setString(1, user.getFullname());
@@ -40,6 +41,13 @@ public class UserDatabase {
            
            pt.executeUpdate();
            set = true;
+           
+           PreparedStatement pt1 = this.con.prepareStatement(query1);
+           pt1.setString(1, user.getEmail());
+           pt1.setString(2, user.getPassword());
+           
+           pt1.executeUpdate();
+           set = true;
         }
         
         catch(SQLException e){
@@ -48,13 +56,35 @@ public class UserDatabase {
         return set;
     }
     
-    public User logUser (String username, String password) {
+    
+    // Admin Login
+    public User logAdmin (String username, String password) {
         
         User user = null;
         try {
             String query = "select * from admin where username=? and password=?";
             PreparedStatement pst = this.con.prepareStatement(query);
             pst.setString(1, username);
+            pst.setString(2, password);
+            
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                user = new User();
+            }
+        }
+        catch (SQLException e) {
+        }
+        
+        return user;
+    }
+    
+    public User logUser (String email, String password) {
+        
+        User user = null;
+        try {
+            String query = "select * from users where email=? and password=?";
+            PreparedStatement pst = this.con.prepareStatement(query);
+            pst.setString(1, email);
             pst.setString(2, password);
             
             ResultSet rs = pst.executeQuery();
