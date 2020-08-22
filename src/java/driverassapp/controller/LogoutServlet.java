b519eebf9b98262c75587da7c601d66cbea78026
Ammar_Fahmy <40536975+AmmarFahmy@users.noreply.github.com>
2020-08-22 21:16:21 +0530

    User and Admin Dashboard added

--- src/java/driverassapp/controller/LogoutServlet.java
+++ src/java/driverassapp/controller/LogoutServlet.java
@@ -19,27 +19,21 @@ import javax.servlet.http.HttpSession;
  */
 public class LogoutServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        
+//        HttpSession session = request.getSession(false);
+//        if (session!=null) {
+//            
+//            //HttpSession session = request.getSession();
+//            session.invalidate(); 
+//            session.removeAttribute("logUser");
+//            response.sendRedirect("index.jsp");
+//        }
+//        
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LogoutServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LogoutServlet at " + request.getContextPath() + "</h1>");
             
             HttpSession session = request.getSession();
             session.removeAttribute("logUser");
@@ -47,46 +41,35 @@ public class LogoutServlet extends HttpServlet {
             session.invalidate();  
             response.sendRedirect("index.jsp");
 
-            
-            out.println("</body>");
-            out.println("</html>");
         }
+
+
+
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
+        
+//        HttpSession session = request.getSession(false);
+//        if (session!=null) {
+//            
+//            //HttpSession session = request.getSession();
+//            session.invalidate(); 
+//            session.removeAttribute("logUser");
+//            response.sendRedirect("index.jsp");
+//        }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
