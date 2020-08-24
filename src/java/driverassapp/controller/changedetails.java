7f5be1e7a9ff35d129b4941effc40a3cc83a79a5
Ammar_Fahmy <40536975+AmmarFahmy@users.noreply.github.com>
2020-08-24 22:31:48 +0530

    User and Admin Functions Finalized

---
+++ src/java/driverassapp/controller/changedetails.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package driverassapp.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ammar
+ */
+public class changedetails extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String clickbtn=request.getParameter("reqbtn");
+            switch(clickbtn){
+                case "Change Details":
+//            String fullname=request.getParameter("fullname");
+            String email=request.getParameter("email");
+            String membershipfee=request.getParameter("membershipfee");
+            String paidorunpaid=request.getParameter("paidorunpaid");
+            System.out.println(membershipfee+paidorunpaid);
+            int i=driverassapp.model.usermodel.updatedetails(membershipfee, paidorunpaid, email);
+            if(i!=0){
+                response.sendRedirect("allPayments.jsp?change=success");
+            }else{
+                response.sendRedirect("allPayments.jsp?changew=fail");
+            }
+            break;
+            case "Go to All Member panel":
+                 response.sendRedirect("admin.jsp");
+                 break;
+            }
+           
+        }    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(changedetails.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(changedetails.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
