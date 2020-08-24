1ee010588ed386511e0ad9c483fbbdcf88d8c8f1
Ammar_Fahmy <40536975+AmmarFahmy@users.noreply.github.com>
2020-08-24 22:31:20 +0530

    User and Admin Functions Finalized

--- src/java/driverassapp/controller/LoginServletUser.java
+++ src/java/driverassapp/controller/LoginServletUser.java
@@ -34,14 +34,6 @@ public class LoginServletUser extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServletUser</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServletUser at " + request.getContextPath() + "</h1>");
             
             String logUserName = request.getParameter("email");
             String logPass = request.getParameter("password");
